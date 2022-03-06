ed0c9a8ded1bd13f5ab79968aedf8d21b44bc0e7
Shubhendu Paul <shubhendu-kumar@users.noreply.github.com>
2022-03-06 17:27:09 +0530

    Added more customization

--- api-gateway/src/main/java/com/spde/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/spde/gateway/ApiGatewayApplication.java
@@ -25,11 +25,23 @@ public class ApiGatewayApplication {
     @Bean
     public Customizer<ReactiveResilience4JCircuitBreakerFactory> defaultCustomizer() {
         return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+        .circuitBreakerConfig(CircuitBreakerConfig.custom()
+           .slidingWindowSize(5)
+           .permittedNumberOfCallsInHalfOpenState(5)
+           .failureRateThreshold(50.0F)
+           .waitDurationInOpenState(Duration.ofMillis(50))
+           .slowCallDurationThreshold(Duration.ofMillis(200))
+           .slowCallRateThreshold(50.0F)
+           .build())
+        .build());
+        /*
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
                 .circuitBreakerConfig(CircuitBreakerConfig.custom()
                         .slidingWindowSize(10)
                         .failureRateThreshold(66.6F)
                         .build()) //.ofDefaults())
                 .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(200)).build())
                 .build());
+         */
     }
 }
