826f98f4b87b240af93ae566bc3a7e1bea4f2dd5
shubhendu-paul <shubhendu.kumar@gmail.com>
2022-03-25 13:34:43 +0530

    Add Docker support to Config-Server module

--- config-server/src/test/java/com/spde/configserver/ConfigServerApplicationTests.java
+++ config-server/src/test/java/com/spde/configserver/ConfigServerApplicationTests.java
@@ -1,11 +1,12 @@
 package com.spde.configserver;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-
-@SpringBootTest(classes = ConfigServerApplication.class, webEnvironment = RANDOM_PORT)
+@SpringBootTest // (classes = ConfigServerApplication.class, webEnvironment = RANDOM_PORT)
+@RunWith(SpringRunner.class)
 class ConfigServerApplicationTests {
 
     @Test
